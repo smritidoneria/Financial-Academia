2844918b3240eb7afdba770d19705ac09ae18d42
Smriti Doneria <smritidoneria@gmail.com>
2025-12-29 14:36:58 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -46,112 +46,112 @@ api.interceptors.response.use(
 
 class Api {
   static async registerUser(data) {
-    return await api.post("api/user/register", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/register", data);
   }
 
   static async loginUser(data) {
-    return await api.post("api/user/login", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/login", data);
   }
   static async incrcoins(data) {
-    return await api.post("api/user/increase-coins", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/increase-coins", data);
   }
   static async decrcoins(data) {
-    return await api.post("api/user/decrease-coins", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/decrease-coins", data);
   }
 
   static async verifyOtp(data) {
-    return await api.post("api/user/verify-otp", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/verify-otp", data);
   }
 
   static async editUser(data) {
-    return await api.post("api/user/update", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/update", data);
   }
 
   static async getUser(data) {
-    return await api.post("api/user/get-user", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/get-user", data);
   }
 
   static async addToInventory(data) {
-    return await api.post("api/user/add-to-inventory", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/add-to-inventory", data);
   }
 
   static async quizSubmission(data) {
-    return await api.post("api/user/save-quiz", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/save-quiz", data);
   }
   static async checkQuiz(data) {
-    return await api.post("api/user/check-quiz", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/check-quiz", data);
   }
 
   static async removeFromInventory(data) {
-    return await api.post("api/user/remove-from-inventory", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/remove-from-inventory", data);
   }
 
   static async getInventory() {
-    return await api.get("api/user/get-inventory");
+    return await api.get("https://financial-academia-mjat.vercel.app/api/user/get-inventory");
   }
 
   static async addTask(data) {
-    return await api.post("api/user/add-task", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/add-task", data);
   }
 
   static async setTaskStatus(data) {
-    return await api.post("api/user/set-task-status", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/set-task-status", data);
   }
 
   static async deleteTask(data) {
-    return await api.post("api/user/delete-task", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/delete-task", data);
   }
 
   static async getTasks() {
-    return await api.get("api/user/get-tasks");
+    return await api.get("https://financial-academia-mjat.vercel.app/api/user/get-tasks");
   }
 
   static async createParty(data) {
-    return await api.post("api/game/create_party", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/game/create_party", data);
   }
 
   static async sendInvite(data) {
-    return await api.post("api/game/send_invite", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/game/send_invite", data);
   }
 
   static async deleteParty(data) {
-    return await api.post("api/game/delete_party", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/game/delete_party", data);
   }
 
   static async acceptInvite(data) {
-    return await api.post("api/game/accept_invite", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/game/accept_invite", data);
   }
 
   static async leaveParty(data) {
-    return await api.post("api/game/leave_party", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/game/leave_party", data);
   }
 
   static async setIncome(data) {
-    return await api.post("api/user/set-income", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/set-income", data);
   }
 
   static async newDayTaskLoad(data) {
-    return await api.post("api/user/new-day-task-load", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/new-day-task-load", data);
   }
 
   static async getAllUsers() {
-    return await api.get("api/user/get-all-users");
+    return await api.get("https://financial-academia-mjat.vercel.app/api/user/get-all-users");
   }
 
   static async getParty(data) {
-    return await api.post("api/game/get_party", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/game/get_party", data);
   }
 
   static async startFight(data) {
-    return await api.post("api/game/start-fight", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/game/start-fight", data);
   }
 
   static async completeFight(data) {
-    return await api.post("api/game/complete-fight", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/game/complete-fight", data);
   }
 
   static async setAvatar(data) {
-    return await api.post("api/user/update-avatar", data);
+    return await api.post("https://financial-academia-mjat.vercel.app/api/user/update-avatar", data);
   }
 }
 
